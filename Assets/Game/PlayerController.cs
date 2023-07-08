0e04782566f757df55a94004a2a4d780c3c2c7cb
Kirill Danilov <insidiousr@yandex.ru>
2023-07-08 10:25:19 +0300

    unitask package, rigidbody scene base

---
+++ Assets/Game/PlayerController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Game {
+	
+	public class PlayerController : MonoBehaviour {
+
+		[SerializeField] private Rigidbody2D m_Rigidbody;
+
+		private float moveSpeed = 5f;
+		private Vector2 moveInput;
+
+		private void OnMove(InputValue input) {
+			moveInput = input.Get<Vector2>();
+		}
+
+		private void FixedUpdate() {
+			m_Rigidbody.velocity = moveInput * moveSpeed;
+		}
+
+	}
+	
+}
