c45fa00f0e7e572de41c90f34f87307cf54927f4
OIegator <olegshev506@gmail.com>
2023-07-09 19:38:48 +0300

    Balance

--- Assets/Game/PlayerController.cs
+++ Assets/Game/PlayerController.cs
@@ -22,7 +22,7 @@ namespace Game {
 		[SerializeField] private float Ability3Cooldown;
 		[SerializeField] private float Ability4Cooldown;
 
-		private float moveSpeed = 1.2f;
+		private float moveSpeed = 2.3f;
 		private Vector2 moveInput;
 		private Vector2 mouseInput;
 
